906b36c15641a807ee450b847046466f6d1926b7
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-02-07 10:49:48 -0500

    progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetch('http://localhost:3000/toys')
+    .then (resp => resp.json())
+    .then (toys => {
+      toys.forEach(toyObj => {
+        console.log(toyObj)
+        const div = document.createElement ('div')
+        document.getElementById("toy-collection").appendChild(div)
+        div.className = "card" 
+        const h2 = document.createElement ('h2')
+        const img = document.createElement ('img')
+        const p = document.createElement ('p')
+        const btn = document.createElement('button')
+        
+        div.appendChild(h2)
+        div.appendChild(img)
+        div.appendChild(p)
+        div.appendChild(btn)
+
+        btn.innerText = "Like"
+        h2.innerText = 
+        // img.src = [toy_image_url]
+        // console.log(h2)
+        // p.innerText = 
+
+      })
+    })  
+    
+    
+
+  
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +42,5 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
 });
